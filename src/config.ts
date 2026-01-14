3e107a0891739f6d21c5ffdf773cbbe4f5152704
whoisjonah <dcps.jonahlgluck@gmail.com>
2026-01-14 15:48:45 -0500

    chore(config): set groundTrafficRevealZoom default to 4

--- src/config.ts
+++ src/config.ts
@@ -7,7 +7,7 @@ export default {
     labelScale: 1,
     // When the basemap scale (zoom) is >= this value, ground traffic will be shown
     // even if `hideGroundTraffic` is true.
-    groundTrafficRevealZoom: 3,
+    groundTrafficRevealZoom: 4,
     // Debug: when true, always show ground traffic regardless of hide flag or zoom
     forceShowGroundTraffic: false,
     // Automatically toggle `hideGroundTraffic` based on zoom crossing the threshold
